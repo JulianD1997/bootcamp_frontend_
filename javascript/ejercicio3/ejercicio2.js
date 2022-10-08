82aee3e1213f7023aeea7dc1860eed4da545c146
Julian Casallas <julian.casallasb@gmail.com>
2022-10-07 22:10:07 -0300

    solucion segundo ejercicio

---
+++ javascript/ejercicio3/ejercicio2.js
@@ -0,0 +1,15 @@
+let nombre = "Julian Casallas"
+let edad = 25
+let dev = true
+let birth = new Date(1997,10,17)
+let libro = {
+    titulo:"El secreo",
+    autor:"Rhonda Byrne",
+    fecha: new Date(2006,10,17),
+    url:"https://www.casadellibro.com/libro-el-secreto/9788479536442/1136037"
+}
+console.log(`nombre :${nombre}`)
+console.log(`edad : ${edad}`)
+console.log(`Es programador : ${dev}`)
+console.log(`fecha de cumpleaños : ${birth}`)
+console.log(libro)
