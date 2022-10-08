26924a772d43dac9df9ea991f7f98591c228795c
Julian Casallas <julian.casallasb@gmail.com>
2022-10-08 19:43:30 -0300

    ejercicio strings

---
+++ javascript/ejercicio6/strings.js
@@ -0,0 +1,31 @@
+const nameStudent = "julian"
+console.log(nameStudent)
+
+const lastName = "casallas"
+console.log(lastName)
+
+let student = `${nameStudent} ${lastName}`
+console.log(student)
+
+let studentUpper = student.toUpperCase()
+console.log(studentUpper)
+
+let studentLower = student.toLowerCase()
+console.log(studentLower)
+
+let long = student.length
+console.log(long)
+
+let firstLetterName = nameStudent.charAt(0)
+console.log(firstLetterName)
+
+let lastLetterLastName = lastName.slice(-1)
+console.log(lastLetterLastName)
+
+let notSpaces = student.replace(" ","")
+console.log(notSpaces)
+
+let nameInString = student.includes(nameStudent)
+console.log(nameInString)
+
+
